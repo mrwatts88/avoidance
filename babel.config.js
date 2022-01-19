c1b79e1b3f23b686f7f9fad5363b4e085ec03f31
Matt Watts <matt@yoketeam.com>
2022-01-18 21:09:38 -0800

    Add react navigator, eslint, and absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
+// eslint-disable-next-line no-undef
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    alias: {
+                        components: './components',
+                        screens: './screens'
+                    },
+                },
+            ],
+        ],
+    }
+}
